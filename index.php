f52b76c2c9601a4aeb1efeaa89b5336db242d678
amusso01 <eandram@hotmail.it>
2016-12-18 18:51:48 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<!--Musso Andrea 09-12-2016 FMA
+Student number : 13026841
+Course : Web Programming using PHP
+Tutor : Tobi Brodie  -->
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Andrea Musso">
+        <title>Web Programming using PHP: FMA</title>
+    </head>
+    <body>
+     <header role="banner">
+
+     </header>
+     <main role="main">
+        <?php
+
+         ?>
+     </main>
+     <footer>
+        
+     </footer>
+    </body>
+</html>
